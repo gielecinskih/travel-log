d8972440f7be2060df22f5fdf249095a27245bfc
Hubert Gieleciński <gielaktv@gmail.com>
2025-07-23 19:57:18 +0200

    feat: tl11 (#50)

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <nuxt-link to="/" class="btn btn-ghost text-xl">
+        Travel log
+      </nuxt-link>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign In With Github
+        <icon name="tabler:brand-github" size="24" />
+      </button>
+    </div>
+  </div>
+</template>
