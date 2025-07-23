184439e1002a7a4d3a3c22a0b698ba64e0669ec7
Hubert <gielaktv@gmail.com>
2025-07-23 19:55:21 +0200

    feat: tl11

--- app.vue
+++ app.vue
@@ -1,8 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-    <button class="btn btn-secondary">
-      <Icon name="tabler:map-star" style="color: white" /> Secondary
-    </button>
-  </h1>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Discover and share your travel experiences with the world. Join our community of travelers and start logging your adventures today!
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
