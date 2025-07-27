2048e28b5787f1f9602daf457b8a3d8dcb605869
Hubert <gielaktv@gmail.com>
2025-07-27 20:05:04 +0200

    feat: tl-17 / tl-18

---
+++ pages/sign-out.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+onMounted(async () => {
+  await authStore.signOut();
+  navigateTo("/");
+});
+</script>
+
+<template>
+  <div class="card  container mt-4 min-h-72 text-center mx-auto flex flex-col items-center justify-center gap-4">
+    <span class="loading loading-dots loading-xl" />
+  </div>
+</template>
