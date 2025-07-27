b3c6609b597e0a35a0fc98cbe48f2ec41f45da33
Hubert <gielaktv@gmail.com>
2025-07-27 20:55:07 +0200

    tl-19 :

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -8,7 +12,10 @@
         <p class="py-6">
           Discover and share your travel experiences with the world. Join our community of travelers and start logging your adventures today!
         </p>
-        <auth-button />
+        <auth-button v-if="!authStore.user" />
+        <nuxt-link v-else to="/dashboard" class="btn btn-primary">
+          Go to Dashboard
+        </nuxt-link>
       </div>
     </div>
   </div>
