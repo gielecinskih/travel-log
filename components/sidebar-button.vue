7b99174d40914d36f9421352db33e7fa4b6ef287
Hubert <gielaktv@gmail.com>
2025-07-28 18:41:55 +0200

    tl-21

---
+++ components/sidebar-button.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const { href } = defineProps<{
+  icon: string;
+  label: string;
+  href: string;
+  showLabel: boolean;
+}>();
+
+const route = useRoute();
+const isActive = computed(() => route.path === href);
+</script>
+
+<template>
+  <div class="tooltip  tooltip-right" :data-tip="showLabel ? undefined : label">
+    <nuxt-link :to="href" :class="{ 'bg-base-200': isActive, '!justify-center': !showLabel }" class="flex p-2 gap-2 hover:bg-base-300 hover:cursor-pointer justify-start items-center">
+      <Icon :name="icon" size="24" />
+      <transition name="grow">
+        <span v-if="showLabel" class="overflow-hidden block text-nowrap">{{ label }}</span>
+      </transition>
+    </nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.3s;
+}
+.grow-leave-active {
+  animation: grow 0.3s reverse;
+}
+@keyframes grow {
+  0% {
+    width: 0%;
+  }
+  100% {
+    width: 100%;
+  }
+}
+</style>
