59292856ad53143a2a22dd8fe0ac24188afffd04
Hubert Gieleciński <gielaktv@gmail.com>
2025-07-28 18:45:45 +0200

    tl-21 (#59)

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,5 +1,32 @@
+<script setup lang="ts">
+const isSideBarOpen = ref(true);
+
+function toggleSidebar() {
+  isSideBarOpen.value = !isSideBarOpen.value;
+  localStorage.setItem("sidebarOpen", isSideBarOpen.value.toString());
+}
+
+onMounted(() => {
+  isSideBarOpen.value = localStorage.getItem("sidebarOpen") !== "false";
+});
+</script>
+
 <template>
-  <div>
-    You are logged in
+  <div class="flex-1 flex">
+    <div class="bg-base-100 w-16 transition-all duration-300" :class="{ 'w-64': isSideBarOpen }">
+      <div class="flex justify-end cursor-pointer hover:bg-base-200 p-2" :class="{ '!justify-center': !isSideBarOpen }" @click="toggleSidebar">
+        <Icon v-if="isSideBarOpen" name="tabler:chevron-left" size="32" />
+        <Icon v-else name="tabler:chevron-right" size="32" />
+      </div>
+      <div class="flex flex-col ">
+        <sidebar-button icon="tabler:map" label="Locations" href="/dashboard" :show-label="isSideBarOpen" />
+        <sidebar-button icon="tabler:circle-plus-filled" label="Add location" href="/dashboard/add" :show-label="isSideBarOpen" />
+        <div class="divider" />
+        <sidebar-button icon="tabler:logout-2" label="Sign Out" href="/sign-out" :show-label="isSideBarOpen" />
+      </div>
+    </div>
+    <div class="flex-1 ">
+      cos
+    </div>
   </div>
 </template>
