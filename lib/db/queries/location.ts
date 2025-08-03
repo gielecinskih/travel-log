67fcd881661e105aaa46545048d577e372ff8942
Hubert Gieleciński <gielaktv@gmail.com>
2025-08-03 18:32:46 +0200

    feat: tl-23 / tl-24 (#61)

---
+++ lib/db/queries/location.ts
@@ -0,0 +1,48 @@
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+
+import type { InsertLocationSchema } from "../schema";
+
+import db from "..";
+import { location } from "../schema";
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuwxyz", 5);
+
+export async function findLocationByName(existing: InsertLocationSchema, userId: number) {
+  return await db.query.location.findFirst({
+    where: and(
+      eq(location.name, existing.name),
+      eq(location.userId, userId),
+    ),
+  });
+}
+
+export async function findLocationBySlug(slug: string) {
+  return !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+}
+
+export async function findUniqueSlug(slug: string) {
+  let existing = await findLocationBySlug(slug);
+
+  while (existing) {
+    const idSlug = `${slug}-${nanoid()}`;
+    existing = await findLocationBySlug(idSlug);
+    if (!existing) {
+      return idSlug;
+    }
+  }
+
+  return slug;
+}
+
+export async function insertLocation(insertable: InsertLocationSchema, slug: string, userId: number) {
+  const [created] = await db.insert(location).values({
+    ...insertable,
+    slug,
+    userId,
+  }).returning();
+
+  return created;
+}
